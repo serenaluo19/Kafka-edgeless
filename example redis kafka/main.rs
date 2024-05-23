8fcedabfd06ba44b76d22c94e3912b792a81cb9f
serenaluo19 <115238914+serenaluo19@users.noreply.github.com>
2024-05-23 18:32:49 +0200

    Add files via upload

---
+++ example redis kafka/main.rs
@@ -0,0 +1,10 @@
+use counter::Counter;  //uso la mia lib counter
+
+fn main() {
+    // Inizializzazione dell'applicazione o utilizzo della libreria
+    let counter = Counter::new();  // Esempio di inizializzazione di un oggetto Counter
+
+    // Esempio di utilizzo di un metodo della tua libreria
+    counter.start();
+
+}
